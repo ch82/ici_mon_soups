182aae1c1662c2243df4abe0f9132585594236fc
Cherkendov Alexander <cherkendov@gmail.com>
2013-02-13 16:19:00 +0400

    IE fixes

--- js/logs.js
+++ js/logs.js
@@ -1,5 +1,5 @@
 function parselogEntry(logEntry){
-	rexp = /^HOST ALERT: (.+?);(DOWN|UP);(SOFT|HARD);\d+;.+$/.exec(logEntry.log_entry)
+	var rexp = /^HOST ALERT: (.+?);(DOWN|UP);(SOFT|HARD);\d+;.+$/.exec(logEntry.log_entry)
 	return {'time':logEntry.timestamp,'host':rexp[1],'state':rexp[2]}
 }
 
@@ -11,11 +11,11 @@ function getLog(start,end,order,limit){
 		if (!limit) { limit = 1000000 }	
 		return "/cgi-bin/icinga/showlog.cgi?ts_start="+ start+"&num_displayed="+limit+"&order="+order+"&ts_end="+end+"&query_string=HOST+ALERT&timeperiod=custom&noti=off&hst=on&sst=off&cmd=off&sms=off&evh=off&flp=off&dwn=off&jsonoutput";
 	}
-	logs = {};
+	var logs = {};
 	$.ajax({dataType:"json", url:generateURL(), async:false, success:function(j){
-		rawlog = j.showlog.log_entries
+		var rawlog = j.showlog.log_entries
 		for (le in rawlog){
-			l = parselogEntry(rawlog[le]);
+			var l = parselogEntry(rawlog[le]);
 			if (!(l.host in logs)) {
 				logs[l.host] = [];
 			}
@@ -25,7 +25,7 @@ function getLog(start,end,order,limit){
 	return logs;
 }
 function getDayLog(day) {
-	start = day.valueOf()/1000;
+	var start = day.valueOf()/1000;
 	return getLog( start, start + 86400)
 }
 $(document).ready(function(){
