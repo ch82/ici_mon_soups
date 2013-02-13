010dbe488b92bdfff9d401c7fb17b11db22e64c6
Cherkendov Alexander <cherkendov@gmail.com>
2013-02-13 15:57:46 +0400

    logs can be viewed now

--- js/logs.js
+++ js/logs.js
@@ -2,14 +2,15 @@ function parselogEntry(logEntry){
 	rexp = /^HOST ALERT: (.+?);(DOWN|UP);(SOFT|HARD);\d+;.+$/.exec(logEntry.log_entry)
 	return {'time':logEntry.timestamp,'host':rexp[1],'state':rexp[2]}
 }
-function generateURL(start,end,order,limit){
-	if (!start) { start = '0' }
-	if (!end) { end = '2147483647' }
-	if (!order) { order = "old2new" }
-	if (!limit) { limit = 1000000 }	
-	return "/cgi-bin/icinga/showlog.cgi?ts_start="+ start+"&num_displayed="+limit+"&order="+order+"&ts_end="+end+"&query_string=HOST+ALERT&timeperiod=custom&noti=off&hst=on&sst=off&cmd=off&sms=off&evh=off&flp=off&dwn=off&jsonoutput";
-}
-function getLog(){
+
+function getLog(start,end,order,limit){
+	function generateURL(){
+		if (!start) { start = '0' }
+		if (!end) { end = '2147483647' }
+		if (!order) { order = "old2new" }
+		if (!limit) { limit = 1000000 }	
+		return "/cgi-bin/icinga/showlog.cgi?ts_start="+ start+"&num_displayed="+limit+"&order="+order+"&ts_end="+end+"&query_string=HOST+ALERT&timeperiod=custom&noti=off&hst=on&sst=off&cmd=off&sms=off&evh=off&flp=off&dwn=off&jsonoutput";
+	}
 	logs = {};
 	$.ajax({dataType:"json", url:generateURL(), async:false, success:function(j){
 		rawlog = j.showlog.log_entries
@@ -22,6 +23,10 @@ function getLog(){
 		}
 	}})
 	return logs;
+}
+function getDayLog(day) {
+	start = day.valueOf()/1000;
+	return getLog( start, start + 86400)
 }
 $(document).ready(function(){
 	getLog(0,2147483647)
